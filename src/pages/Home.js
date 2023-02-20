a6210452de94218b1f6a027a76e33f0548c4aa4d
Badr Eddine El-Harchali <mr.elharchali@gmail.com>
2023-02-20 22:04:39 +0100

    Famous products section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -221,7 +221,72 @@ const Home = () => {
         </div>
       </section>
 
-      <section className="special-wrapper py-5 home-wrapper-5">
+      <section className="famous-wrapper py-5 home-wrapper-5">
+        <div className="row">
+          <div className="col-3">
+            <div className="famous-card position-relative">
+              <img
+                src="images/famous-1.webp"
+                className="img-fluid"
+                alt="famous"
+              />
+              <div className="famous-content position-absolute">
+                <h5>Big Screen</h5>
+                <h6>Smart Watch Series 7</h6>
+                <p>From $399or $16.62/mo. for 24 mo.*</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-3">
+            <div className="famous-card position-relative">
+              <img
+                src="images/famous-2.webp"
+                className="img-fluid"
+                alt="famous"
+              />
+              <div className="famous-content position-absolute">
+                <h5 className="text-dark">Studio Display</h5>
+                <h6 className="text-dark">600 nits of brightness.</h6>
+                <p className="text-dark">27-inch 5K Retina display</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-3">
+            <div className="famous-card position-relative">
+              <img
+                src="images/famous-3.webp"
+                className="img-fluid"
+                alt="famous"
+              />
+              <div className="famous-content position-absolute">
+                <h5 className="text-dark">smartphones</h5>
+                <h6 className="text-dark">Smartphone 13 Pro.</h6>
+                <p className="text-dark">
+                  Now in Green. From $999.00 or $41.62/mo. for 24 mo. Footnote*
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-3">
+            <div className="famous-card position-relative">
+              <img
+                src="images/famous-3.webp"
+                className="img-fluid"
+                alt="famous"
+              />
+              <div className="famous-content position-absolute">
+                <h5 className="text-dark">home speakers</h5>
+                <h6 className="text-dark">Room-filling sound.</h6>
+                <p className="text-dark">
+                  From $699 or $116.58/mo. for 12 mo.*
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="special-wrapper py-5 home-wrapper-6">
         <div className="container-xxl">
           <div className="row">
             <div className="col-12">
@@ -237,7 +302,7 @@ const Home = () => {
         </div>
       </section>
 
-      <section className="popular-wrapper py-5 home-wrapper-6">
+      <section className="popular-wrapper py-5 home-wrapper-7">
         <div className="container-xxl">
           <div className="row">
             <div className="col-12">
@@ -253,7 +318,7 @@ const Home = () => {
         </div>
       </section>
 
-      <section className="marque-wrapper home-wrapper-7 py-5">
+      <section className="marque-wrapper home-wrapper-8 py-5">
         <div className="container-xxl">
           <div className="row">
             <div className="col-12">
@@ -290,7 +355,7 @@ const Home = () => {
         </div>
       </section>
 
-      <section className="blog-wrapper py-5 home-wrapper-8">
+      <section className="blog-wrapper py-5 home-wrapper-9">
         <div className="container-xxl">
           <div className="row">
             <div className="col-12">
