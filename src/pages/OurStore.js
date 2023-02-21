100437c827495b04ba35180aaca5941ab3a0fe9e
Badr Eddine El-Harchali <mr.elharchali@gmail.com>
2023-02-21 20:17:16 +0100

    Create a Breadcrump for 'Our Store' Page

---
+++ src/pages/OurStore.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import BreadCrumb from '../components/BreadCrumb'
+
+function OurStore() {
+  return (
+    <>
+      <BreadCrumb title="Our Store"/>
+    </>
+  )
+}
+
+export default OurStore
