4160dbf88f04b1c149177654894d3611c5bb9950
Badr Eddine El-Harchali <mr.elharchali@gmail.com>
2023-02-23 17:25:31 +0100

    Refund policy page

--- src/pages/RefundPolicy.js
+++ src/pages/RefundPolicy.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import BreadCrumb from "../components/BreadCrumb";
+import Meta from "../components/Meta";
 
-function RefundPolicy() {
+const RefundPloicy = () => {
   return (
-    <div>RefundPolicy</div>
-  )
-}
+    <>
+      <Meta title={"Refund Policy"} />
+      <BreadCrumb title="Refund Policy" />
+      <section class1="policy-wrapper py-5 home-wrapper-2">
+        <div className="row">
+          <div className="col-12">
+            <div className="policy"></div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default RefundPolicy
+export default RefundPloicy;
