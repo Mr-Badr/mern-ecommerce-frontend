696a77f9ef84d111b8655f332cb805aac6ea8c80
Badr Eddine El-Harchali <mr.elharchali@gmail.com>
2023-02-23 17:24:46 +0100

    Privacy policy page

--- src/pages/PrivacyPolicy.js
+++ src/pages/PrivacyPolicy.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import BreadCrumb from "../components/BreadCrumb";
+import Meta from "../components/Meta";
 
-function PrivacyPolicy() {
+const PrivacyPolicy = () => {
   return (
-    <div>PrivacyPolicy</div>
-  )
-}
+    <>
+      <Meta title={"Privacy Policy"} />
+      <BreadCrumb title="Privacy Policy" />
+      <section class1="policy-wrapper py-5 home-wrapper-2">
+        <div className="row">
+          <div className="col-12">
+            <div className="policy"></div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default PrivacyPolicy
+export default PrivacyPolicy;
