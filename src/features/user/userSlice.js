5cc6cf6b9e55e4597db0eb8251432404cd012a1b
Badr Eddine El-Harchali <mr.elharchali@gmail.com>
2023-04-05 18:30:16 +0000

    Redux store setup

---
+++ src/features/user/userSlice.js
@@ -0,0 +1,48 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { authService } from "./userService";
+
+export const registerUser = createAsyncThunk(
+  "auth/register",
+  async (userData, thunkAPI) => {
+    try {
+      return await authService.register(userData);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error)
+    }
+  }
+);
+
+const initialState = {
+  user: "",
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: "",
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState: initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(registerUser.pending, (state) => {
+        // When it is in Loading state, then isLoading is true
+        state.isLoading = true;
+      })
+      .addCase(registerUser.fulfilled, (state, action) => {
+        // With the help of action will get our payload
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+      })
+      .addCase(registerUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        state.message = action.error;
+      });
+  },
+});
+
+export default authSlice.reducer;
